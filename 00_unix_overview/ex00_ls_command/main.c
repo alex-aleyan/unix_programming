df9209ad81131f1fbdec8ef562e14913afabf2ee
Alexander Aleyan <aaleyan@asrcfederal.com>
2021-08-25 10:28:27 -0400

    cleaned up includes

--- 00_unix_overview/ex00_ls_command/main.c
+++ 00_unix_overview/ex00_ls_command/main.c
@@ -1,14 +1,17 @@
+// System:
 #include <stdio.h>
-
-#include <unistd.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <string.h>
-#include <time.h>
-#include <sys/types.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <time.h>
+//#include <sys/types.h>
 
-//#include "../../apue/include/apue.h"
+// Mine:
 #include "../../../common/c_cpp/c/include/func.h"
+
+// Book's:
+//#include "../../apue/include/apue.h"
 #include <dirent.h> // provides dir manipulation funcs and data types.
 
 
