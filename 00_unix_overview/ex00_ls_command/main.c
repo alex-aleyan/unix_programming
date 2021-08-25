89c47697b5622b3b5947ee1d0bb12b5cf9db0203
Alexander Aleyan <aaleyan@asrcfederal.com>
2021-08-25 09:57:58 -0400

    updated with bailverbose func

--- 00_unix_overview/ex00_ls_command/main.c
+++ 00_unix_overview/ex00_ls_command/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -8,23 +9,29 @@
 
 //#include "../../apue/include/apue.h"
 #include "../../../common/c_cpp/c/include/func.h"
-#include <dirent.h>
+#include <dirent.h> // provides dir manipulation funcs and data types.
 
 
 int main(int argc, char *argv[])
 {
-    DIR *dp;
-    struct dirent *dirp;
+    DIR *dp; // comes from dirent.h line 128
+    struct dirent *dirp; // comes from dirent.h->bits/dirent.h
     
-    if (argc != 2)
-        bail("0: bailing at main() on line __LINE__");
+    if (argc != 2) {
+        //printf("File %s(%d)\n", __FILE__, __LINE__);
+        bailverbose("must pass exactly 1 argument", __FILE__, __LINE__);
         //err_quit("usage: ls directory_name");
+    }
     
-    if ((dp = opendir(argv[1])) == NULL)
-        bail("1: bailing at main() on line __LINE__");
+    if ((dp = opendir(argv[1])) == NULL) { // opendir comes from dirent.h
+        //printf("File %s(%d)\n", __FILE__, __LINE__);
+        bailverbose("failed to open dir", __FILE__, __LINE__);
+        //bail("1: bailing at main() on line __LINE__");
+    }
+
         //err_sys("can’t open %s", argv[1]);
     
-    while ((dirp = readdir(dp)) != NULL)
+    while ((dirp = readdir(dp)) != NULL) // readdir comes from dirent.h
         printf("%s\n", dirp->d_name);
     
     closedir(dp);
