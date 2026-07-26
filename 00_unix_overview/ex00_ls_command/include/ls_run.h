54cd1887d24527caa195ff046e4cfc94fd8e07b1
alex-aleyan <sasha.aleyan@gmail.com>
2026-07-26 14:25:47 -0400

    ex00 - split ls_run.h into ls_run.h and ls_run.c

--- 00_unix_overview/ex00_ls_command/include/ls_run.h
+++ 00_unix_overview/ex00_ls_command/include/ls_run.h
@@ -1,41 +1,6 @@
-// System:
-#include <stdio.h>
-#include <errno.h>
-//#include <unistd.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <time.h>
-//#include <sys/types.h>
+#ifndef LS_RUN
+#define LS_RUN
 
-// Mine:
-// #include "../../../common/c_cpp/c/include/func.h"
-#include "func.h"
+void ls_run(int argc, char *argv[]);
 
-// Book's:
-//#include "../../apue/include/apue.h"
-#include <dirent.h> // provides dir manipulation funcs and data types.
-
-
-void ls_run(int argc, char *argv[])
-{
-    DIR           *dp;      // comes from dirent.h line 128
-    struct dirent *dirp;    // comes from dirent.h->bits/dirent.h
-    
-    if (argc != 2) {
-        bailverbose("must pass exactly 1 argument", __FILE__, __LINE__);
-        //err_quit("usage: ls directory_name");
-        fputs(strerror(errno), stderr);
-    }
-    
-    if ((dp = opendir(argv[1])) == NULL) { // opendir comes from dirent.h
-        fputs(strerror(errno), stderr);
-        bailverbose("failed to open dir", __FILE__, __LINE__);
-        //err_sys("can’t open %s", argv[1]);
-    }
-    
-    while ((dirp = readdir(dp)) != NULL) // readdir comes from dirent.h
-        printf("%s\n", dirp->d_name);
-    
-    // closedir comes from dirent.h
-    closedir(dp);
-}
+#endif
